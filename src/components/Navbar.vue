cb02f34179fae446d36980a2ccdf0fa16b88a7ee
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-09-23 10:59:00 +0800

    More on Router Links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,6 +12,14 @@
           <router-link :to="{ name: 'Contact'}">Contact</router-link>
         </li>
       </ul>
+      <h2>User Profile</h2>
+      <ul>
+        <li v-for="(id, index) in userIds" :key="index">
+          <router-link :to="{ name: 'ViewProfile',params:{ user_id: id}}">
+            <span>Profile {{ id }} </span>
+          </router-link>
+        </li>
+      </ul>
   </nav>
 </template>
 
@@ -20,7 +28,7 @@ export default {
   name: 'Navbar',
   data () {
     return {
-
+      userIds: ['1','2','3','4'],
     }
   }
 }
